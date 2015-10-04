fbb03958ca71844a47caf3084fce55d3a11added
Johannes Holzfuß <DataWraith@fastmail.fm>
2015-10-04 21:29:09 +0200

    Add timeoutbot, an example bot that times out

--- bot_registry.go
+++ bot_registry.go
@@ -5,10 +5,12 @@ import "./vindinium"
 import (
 	"./bots/passivebot"
 	"./bots/randombot"
+	"./bots/timeoutbot"
 )
 
 // BotRegistry maps the names of the bots to actual values
 var BotRegistry = map[string]vindinium.Bot{
 	"passive": &passivebot.Bot{},
 	"random":  &randombot.Bot{},
+	"timeout": &timeoutbot.Bot{},
 }
---
+++ bots/timeoutbot/timeoutbot.go
@@ -0,0 +1,30 @@
+// Package timeoutbot implements a simple bot that times out immediately.
+package timeoutbot
+
+import (
+	"fmt"
+	"time"
+
+	v "../../vindinium"
+)
+
+type Bot struct{}
+
+func (b *Bot) Move(s v.State) v.Direction {
+	time.Sleep(2 * time.Second)
+	return v.Stay
+}
+
+func (b *Bot) EndOfGame(err error, s v.State) {
+	gameID := s.Game.ID
+
+	if err != nil {
+		fmt.Printf("Game %s aborted due to error: %v", gameID, err)
+		return
+	}
+
+	fmt.Printf("Game %s finished:\n", gameID)
+	for _, h := range s.Game.Heroes {
+		fmt.Printf("%4d %s\n", h.Gold, h.Name)
+	}
+}
