96f081dd77ffbec1d002a1ad9467580da80b834f
Johannes Holzfuß <DataWraith@fastmail.fm>
2015-10-04 16:02:37 +0200

    Define commandline flags

--- main.go
+++ main.go
@@ -1,6 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+var (
+	server      string
+	key         string
+	mode        string
+	botname     string
+	numParallel int
+	numGames    int
+
+	shouldExit bool
+)
+
+func init() {
+	flag.StringVar(&server, "s", "http://vindinium.org", "server")
+	flag.StringVar(&key, "k", "", "API key")
+	flag.StringVar(&mode, "m", "training", "game mode (\"arena\" or \"training\")")
+	flag.StringVar(&botname, "b", "random", "name of the bot to use")
+	flag.IntVar(&numParallel, "j", 1, "how many instances of the bot to run in parallel")
+	flag.IntVar(&numGames, "c", 1, "how many games to play (0 for continuous play)")
+	flag.Parse()
+}
 
 func main() {
 	c := &Client{
