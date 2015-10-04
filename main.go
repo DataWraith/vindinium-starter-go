a00326c8849fbc0e96d4158184da0e91025fe2da
Johannes Holzfuß <DataWraith@fastmail.fm>
2015-10-04 20:53:42 +0200

    Check the command-line options for errors

--- main.go
+++ main.go
@@ -15,7 +15,8 @@ var (
 	numParallel int
 	numGames    int
 
-	shouldExit bool
+	shouldExit   bool
+	gamesPlayerd int
 )
 
 func init() {
@@ -23,7 +24,7 @@ func init() {
 	flag.StringVar(&key, "k", "", "API key")
 	flag.StringVar(&mode, "m", "training", "game mode (\"arena\" or \"training\")")
 	flag.StringVar(&botname, "b", "random", "name of the bot to use")
-	flag.IntVar(&numParallel, "j", 1, "how many instances of the bot to run in parallel")
+	flag.IntVar(&numParallel, "j", 1, "how many instances of the bot to run in parallel (arena mode only)")
 	flag.IntVar(&numGames, "c", 1, "how many games to play (0 for continuous play)")
 	flag.Parse()
 }
@@ -44,6 +45,29 @@ func main() {
 			shouldExit = true
 		}
 	}()
+
+	// Set up the options correctly
+	if key == "" {
+		fmt.Println("You must provide an API key with the -k command-line option.")
+		os.Exit(1)
+	}
+
+	if mode == "training" {
+		numParallel = 1
+	} else if mode == "arena" {
+		// Do nothing
+	} else {
+		fmt.Printf("Unrecognized mode: %q. Mode must be \"arena\" or \"training\".\n", mode)
+		os.Exit(1)
+	}
+
+	bot, ok := BotRegistry[botname]
+	if !ok {
+		fmt.Printf("The bot name %q is not registered. To register a new bot, edit `bog_registry.go` and recompile the client with `go build`.\n", botname)
+		os.Exit(1)
+	}
+
+	_ = bot
 
 	c := &Client{
 		Server:    "http://vindinium.org",
