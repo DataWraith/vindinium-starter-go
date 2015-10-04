71c8de09174484979652cf776ffc521483c013f7
Johannes Holzfuß <DataWraith@fastmail.fm>
2015-10-04 14:09:15 +0200

    Direction: Add an array containing the cardinal Directions

--- vindinium/direction.go
+++ vindinium/direction.go
@@ -1,5 +1,6 @@
 package vindinium
 
+// Direction represents one of the possible movement directions in Vindinium
 type Direction string
 
 const (
@@ -9,3 +10,7 @@ const (
 	West  Direction = "West"
 	Stay  Direction = "Stay"
 )
+
+// NESW is an array containing the cardinal directions for convenient use with
+// a range statement.
+var NESW = [4]Direction{North, East, South, West}
