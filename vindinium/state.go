a298657e479a53ee5d9f7235ec0dae76638ea3d1
Johannes Holzfuß <DataWraith@fastmail.fm>
2015-10-05 09:23:19 +0200

    State: Add an informative comment

--- vindinium/state.go
+++ vindinium/state.go
@@ -1,6 +1,7 @@
 package vindinium
 
-// State represents the current state of a game of Vindinium.
+// State represents the current state of a game of Vindinium. This is a
+// direct representation of the JSON that is returned by the server.
 type State struct {
 	Game    Game
 	Hero    Hero
